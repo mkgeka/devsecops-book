cb24388fc31e26badded69ea26bbb42ac5cbefa7
Ievgenii Bloshchanevych <mkgeka@gmail.com>
2024-04-05 12:48:52 -0400

    update IAM role

--- chapter-2/scripts/chapter-2.ps1
+++ chapter-2/scripts/chapter-2.ps1
@@ -1,6 +1,6 @@
 ###
 aws cloudformation create-stack --template-body file://chapter-2/template/ServiceCatalogPortfolio.yml --stack-name EKSPortfolio --parameters file://chapter-2/parameter/ServiceCatalog.json --capabilities CAPABILITY_NAMED_IAM --region ca-central-1
-aws s3 cp chapter-2/product/ s3://mkgeka-devopssec/ --recursive --exclude "*" --include "*.yml" --include "*.zip" --region ca-central-1
+#aws s3 cp chapter-2/product/ s3://mkgeka-devopssec/ --recursive --exclude "*" --include "*.yml" --include "*.zip" --region ca-central-1
 aws cloudformation create-stack --template-body file://chapter-2/template/ServiceCatalogProduct.yml --stack-name EKSNetwork --parameters file://chapter-2/parameter/NetworkProduct.json --region ca-central-1
 aws cloudformation create-stack --template-body file://chapter-2/template/ServiceCatalogProduct.yml --stack-name EKSIAM --parameters file://chapter-2/parameter/IAMProduct.json --region ca-central-1
 aws cloudformation create-stack --template-body file://chapter-2/template/ServiceCatalogProduct.yml --stack-name EKSCluster --parameters file://chapter-2/parameter/EKSProduct.json --region ca-central-1
@@ -10,8 +10,8 @@ aws cloudformation create-stack --template-body file://chapter-2/template/Servic
 aws cloudformation create-stack --template-body file://chapter-2/template/ServiceCatalogProduct.yml --stack-name EKSLog --parameters file://chapter-2/parameter/EKSLoggingProduct.json --region ca-central-1
 $PRODUCTID = aws servicecatalog describe-product --name DEV-eks-provisioner-product-ca-central-1 --region ca-central-1 --query ProductViewSummary.ProductId --output text
 $PROVISIONARTIFACTID = aws servicecatalog describe-product --name DEV-eks-provisioner-product-ca-central-1 --region ca-central-1 --query 'ProvisioningArtifacts[0].Id' --output text
-aws servicecatalog provision-product --provisioned-product-name "eks-provision" --provisioning-artifact-id $PROVISIONARTIFACTID --product-id $PRODUCTID --provisioning-parameters file://chapter-2/parameter/eks-provisioner.json --profile default --region ca-central-1
-aws cloudformation delete-stack --stack-name EKSPortfolio --region ca-central-1
-aws cloudformation delete-stack --stack-name EKSNetwork --region ca-central-1
-aws cloudformation delete-stack --stack-name EKSIAM --region ca-central-1
-aws cloudformation delete-stack --stack-name EKSCluster --region ca-central-1
+#aws servicecatalog provision-product --provisioned-product-name "eks-provision" --provisioning-artifact-id $PROVISIONARTIFACTID --product-id $PRODUCTID --provisioning-parameters file://chapter-2/parameter/eks-provisioner.json --profile default --region ca-central-1
+#aws cloudformation delete-stack --stack-name EKSPortfolio --region ca-central-1
+#aws cloudformation delete-stack --stack-name EKSNetwork --region ca-central-1
+#aws cloudformation delete-stack --stack-name EKSIAM --region ca-central-1
+#aws cloudformation delete-stack --stack-name EKSCluster --region ca-central-1
